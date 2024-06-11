f42a65fc96384c7c741c76f8ce2257ce07200c56
ivanvinicius <ivanvinicius@github.com>
2024-06-11 19:10:50 -0300

    store profile dialog

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,6 +1,12 @@
+import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
 
+import { getManagedRestaurant } from '~/api/get-managed-restaurant'
+import { getProfile } from '~/api/get-profile'
+
+import { StoreProfileDialog } from './store-profile-dialog'
 import { Button } from './ui/button'
+import { Dialog, DialogTrigger } from './ui/dialog'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,39 +15,70 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
+import { Skeleton } from './ui/skeleton'
 
 export function AccountMenu() {
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurants } =
+    useQuery({
+      queryKey: ['managed-restaurant'],
+      queryFn: getManagedRestaurant,
+    })
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          className="flex select-none items-center gap-2"
-          variant={'outline'}
-        >
-          Pizza Shop
-          <ChevronDown className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            className="flex select-none items-center gap-2"
+            variant={'outline'}
+          >
+            {isLoadingManagedRestaurants ? (
+              <Skeleton className="h-4 w-40" />
+            ) : (
+              managedRestaurant?.name
+            )}
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent className="w-56" align="end">
-        <DropdownMenuLabel className="flex flex-col">
-          <span>Ivan Vinicius</span>
-          <span className="text-xs font-normal text-muted-foreground">
-            ivan@rocketseat.com.br
-          </span>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
+        <DropdownMenuContent className="w-56" align="end">
+          <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ? (
+              <div className="space-y-1">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-4 w-40" />
+              </div>
+            ) : (
+              <div className="space-y-1">
+                <span>{profile?.name}</span>
+                <span className="block text-xs font-normal text-muted-foreground">
+                  {profile?.email}
+                </span>
+              </div>
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
 
-        <DropdownMenuItem className="flex gap-2">
-          <Building className="h-4 w-4" />
-          <span>Perfil da loja</span>
-        </DropdownMenuItem>
+          <DialogTrigger asChild>
+            <DropdownMenuItem className="flex gap-2">
+              <Building className="h-4 w-4" />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
 
-        <DropdownMenuItem className="flex gap-2 text-rose-500 dark:text-rose-400">
-          <LogOut className="h-4 w-4" />
-          <span>Sair</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem className="flex gap-2 text-rose-500 dark:text-rose-400">
+            <LogOut className="h-4 w-4" />
+            <span>Sair</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <StoreProfileDialog />
+    </Dialog>
   )
 }
