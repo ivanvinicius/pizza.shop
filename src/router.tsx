e0d9c100091f9130eda1d217d76fcd7203a54e0a
ivanvinicius <ivanvinicius@github.com>
2024-06-12 19:12:45 -0300

    error page

--- src/router.tsx
+++ src/router.tsx
@@ -7,12 +7,13 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignUp } from './pages/auth/sign-up'
 import { SignIn } from './pages/auth/sing-in'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   /* auth */
   {
     element: <AuthLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: '/sign-up', element: <SignUp /> },
       { path: '/sign-in', element: <SignIn /> },
@@ -22,10 +23,16 @@ export const router = createBrowserRouter([
   /* dashboard */
   {
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
     ],
+  },
+
+  /* catch all not found routes */
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
